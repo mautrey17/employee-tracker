5139cde5c6c11628aa414a83bcddd6512d1429cd
Matt Autrey <mautrey17@gmail.com>
2021-02-26 22:30:23 -0500

    establish connection with sql

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "1234",
+  database: "employee_trackerdb",
+});
+
+
+
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log(`connect to db as id ${conn.threadId}`);
+});
