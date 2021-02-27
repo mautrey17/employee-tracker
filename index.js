2cfd55f5041ffea8c3b1c725faa0c0bf82c2c0b1
Matt Autrey <mautrey17@gmail.com>
2021-02-27 09:54:55 -0500

    menu question

--- index.js
+++ index.js
@@ -10,7 +10,41 @@ const conn = mysql.createConnection({
 });
 
 
-
+function menu() {
+    inquirer.prompt({
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['Add an employee', 'Add a department', 'Add a role', 'View departments', 'View roles', 'View employees', 'Update employee roles'],
+        name: 'desiredAction'
+    }).then(response => {
+        switch (response.desiredAction){
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'View a department':
+                viewDepartment();
+                break;
+            case 'View an employee':
+                viewEmployee();
+                break;
+            case 'View a role':
+                viewRole();
+                break;
+            case 'Add a role':
+                updateEmployeeRole();
+                break;
+            default:
+                console.log('Goodbye!');
+                conn.end();
+        }
+    })
+}
 
 conn.connect((err) => {
   if (err) throw err;
